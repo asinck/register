5f213c48fa9adb8b1850327fd27e9aa5e6f09632
Adam Sinck <absinck+github@grueslayer.com>
2017-07-30 03:04:57 -0700

    add a customer class and empty unit test

--- java/src/main/Register.java
+++ java/src/main/Register.java
@@ -1262,8 +1262,232 @@ class Model {
      * @return the customer correstponding to the given number, or
      *         null if not found
      */
-    public ReceiptItem itemLookup(int code) {
+    public ReceiptItem customerLookup(int number) {
         return null;
     }
 }
 
+
+/**
+ * The class for a customer's info. Contains basic information.
+ * 
+ * @author Adam Sinck
+ * @version 0.0
+ */
+class Customer {
+    private String addressL1, addressL2, city, state, email;
+    private int zip, phoneNumber;
+    private boolean member, subscribe;
+
+    /**
+     * Class constructor.
+     * 
+     * @param addressL1         Address line 1
+     * @param addressL2         Address line 2
+     * @param city              City
+     * @param state             State
+     * @param zip               Zip code
+     * @param email             Email address
+     * @param phoneNumber       Phone number
+     * @param member            member?
+     * @param subscribe         subscribe?
+     */
+    Customer(String _addressL1, String _addressL2, String _city,
+             String _state, int _zip, String _email, int _phoneNumber,
+             boolean _member, boolean _subscribe) {
+        addressL1 = _addressL1;
+        addressL2 = _addressL2;
+        city = _city;
+        state = _state;
+        zip = _zip;
+        email = _email;
+        phoneNumber = _phoneNumber;
+        member = _member;
+        subscribe = _subscribe;
+    }
+
+
+    //python script ftw
+    // addressL1
+    // addressL2
+    // city
+    // state
+    // zip
+    // email
+    // phoneNumber
+    // member
+    // subscribe;
+
+    // /**
+    //  * The getter function for %s
+    //  * 
+    //  * @return %s
+    //  */
+    // public String get%s() {
+    //     return %s;
+    // }
+    
+    // /**
+    //  * The setter function for %s
+    //  * 
+    //  * @param new%s, the new %s
+    //  */
+    // public String set%s(String new%s) {
+    //     %s = new%s;
+    // }
+
+    /**
+     * The getter function for addressL1
+     * 
+     * @return addressL1
+     */
+    public String getAddressL1() {
+        return addressL1;
+    }
+    
+    /**
+     * The setter function for addressL1
+     * 
+     * @param newAddressL1, the new addressL1
+     */
+    public void setAddressL1(String newAddressL1) {
+        addressL1 = newAddressL1;
+    }
+    /**
+     * The getter function for addressL2
+     * 
+     * @return addressL2
+     */
+    public String getAddressL2() {
+        return addressL2;
+    }
+    
+    /**
+     * The setter function for addressL2
+     * 
+     * @param newAddressL2, the new addressL2
+     */
+    public void setAddressL2(String newAddressL2) {
+        addressL2 = newAddressL2;
+    }
+    /**
+     * The getter function for city
+     * 
+     * @return city
+     */
+    public String getCity() {
+        return city;
+    }
+    
+    /**
+     * The setter function for city
+     * 
+     * @param newCity, the new city
+     */
+    public void setCity(String newCity) {
+        city = newCity;
+    }
+    /**
+     * The getter function for state
+     * 
+     * @return state
+     */
+    public String getState() {
+        return state;
+    }
+    
+    /**
+     * The setter function for state
+     * 
+     * @param newState, the new state
+     */
+    public void setState(String newState) {
+        state = newState;
+    }
+    /**
+     * The getter function for zip
+     * 
+     * @return zip
+     */
+    public int getZip() {
+        return zip;
+    }
+    
+    /**
+     * The setter function for zip
+     * 
+     * @param newZip, the new zip
+     */
+    public void setZip(int newZip) {
+        zip = newZip;
+    }
+    /**
+     * The getter function for email
+     * 
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+    
+    /**
+     * The setter function for email
+     * 
+     * @param newEmail, the new email
+     */
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+    /**
+     * The getter function for phoneNumber
+     * 
+     * @return phoneNumber
+     */
+    public int getPhonenumber() {
+        return phoneNumber;
+    }
+    
+    /**
+     * The setter function for phoneNumber
+     * 
+     * @param newPhoneNumber, the new phoneNumber
+     */
+    public void setPhonenumber(int newPhoneNumber) {
+        phoneNumber = newPhoneNumber;
+    }
+    /**
+     * The getter function for member
+     * 
+     * @return member
+     */
+    public boolean getMember() {
+        return member;
+    }
+    
+    /**
+     * The setter function for member
+     * 
+     * @param newMember, the new member
+     */
+    public void setMember(boolean newMember) {
+        member = newMember;
+    }
+    /**
+     * The getter function for subscribe
+     * 
+     * @return subscribe
+     */
+    public boolean getSubscribe() {
+        return subscribe;
+    }
+    
+    /**
+     * The setter function for subscribe
+     * 
+     * @param newSubscribe, the new subscribe
+     */
+    public void setSubscribe(boolean newSubscribe) {
+        subscribe = newSubscribe;
+    }
+    
+}
---
+++ java/src/test/CustomerTest.java
@@ -0,0 +1,16 @@
+// https://www.tutorialspoint.com/junit/junit_executing_tests.htm
+
+package register;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+	
+    // Customer customer = new Customer();
+    
+    @Test
+    public void testFooter() {
+        assertEquals(1, 1);
+    }
+}
