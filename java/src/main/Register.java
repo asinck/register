c850edaff4564369ec0730117a8795b115cbb1d8
Adam Sinck <absinck+github@grueslayer.com>
2017-07-30 02:40:34 -0700

    add placeholder functions to the model

--- java/src/main/Register.java
+++ java/src/main/Register.java
@@ -1234,9 +1234,36 @@ class CustomerInfo {
  * @version 0.0
  */
 class Model {
-
+    
+    /**
+     * Class constructor.
+     */
     public Model() {
         
+    }
+
+    /**
+     * Looks up the item code in the database and returns the item if
+     * found and null otherwise.
+     * 
+     * @param code      The item code to look up
+     * @return the item correstponding to the given item code, or null
+     *         if not found
+     */
+    public ReceiptItem itemLookup(int code) {
+        return null;
+    }
+    
+    /**
+     * Looks up the customer phone number in the database and returns
+     * the account if found and null otherwise.
+     * 
+     * @param number    The number to look up
+     * @return the customer correstponding to the given number, or
+     *         null if not found
+     */
+    public ReceiptItem itemLookup(int code) {
+        return null;
     }
 }
 
