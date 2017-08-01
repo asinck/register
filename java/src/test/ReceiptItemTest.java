171ae96f66bdf3ace3d03e2c805d15ef90729a44
Adam Sinck <absinck+github@grueslayer.com>
2017-08-01 10:29:28 -0700

    update receipt item tests

--- java/src/test/ReceiptItemTest.java
+++ java/src/test/ReceiptItemTest.java
@@ -4,10 +4,18 @@ package register;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 
 public class ReceiptItemTest {
     ReceiptItem eggs = new ReceiptItem("eggs", 1, 3.33);
+    
+    ReceiptItem A1 = new ReceiptItem("A", 1, 3.33);
+    ReceiptItem A2 = new ReceiptItem("A", 2, 3.33);
+    ReceiptItem A3 = new ReceiptItem("A", 1, 1.00);
+    ReceiptItem B1 = new ReceiptItem("B", 1, 3.33);
+    ReceiptItem B2 = new ReceiptItem("B", 2, 3.33);
+    ReceiptItem B3 = new ReceiptItem("B", 1, 1.00);
     
     @Test
     public void testReceiptItem() {
@@ -36,5 +44,33 @@ public class ReceiptItemTest {
         //make sure the total is consistent with itself
         assertEquals(eggs.getCount() * eggs.getPrice(),
                      eggs.getTotal(), marginOfError);
+
+        //test .equals
+        /*
+          A1 == A2
+          B1 == B2
+          
+          The rest aren't equal.
+         */
+        assertEquals(A1, A2);
+        assertEquals(B1, B2);
+
+        assertNotEquals(A1, A3);
+        assertNotEquals(A2, A3);
+
+        assertNotEquals(B1, B3);
+        assertNotEquals(B2, B3);
+        
+        assertNotEquals(A1, B1);
+        assertNotEquals(A1, B2);
+        assertNotEquals(A1, B3);
+
+        assertNotEquals(A2, B1);
+        assertNotEquals(A2, B2);
+        assertNotEquals(A2, B3);
+
+        assertNotEquals(A3, B1);
+        assertNotEquals(A3, B2);
+        assertNotEquals(A3, B3);
     }
 }
