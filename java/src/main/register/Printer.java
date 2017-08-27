d3493f54f5a3f2c1a4831f037fd811c988b0fae3
Adam Sinck <absinck+github@grueslayer.com>
2017-08-26 21:47:01 -0700

    make the printer simulation a GUI

--- java/src/main/register/Printer.java
+++ java/src/main/register/Printer.java
@@ -1,5 +1,11 @@
 package register;
 
+import java.awt.GridBagLayout;
+import java.awt.TextArea;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+
 /**
  * The class for a printer. This class mimics the function of a printer, due to
  * lack of hardware. This may be converted into a Java Interface or something
@@ -11,7 +17,8 @@ package register;
 class Printer implements HardwareInterface_Printer {
     private ReceiptItem[] list;
     private double taxRate = 0.086;
-
+    private JFrame PrinterWindow;
+    private TextArea output;
     /**
      * Class constructor.
      *
@@ -19,6 +26,18 @@ class Printer implements HardwareInterface_Printer {
      */
     Printer (ReceiptItem[] _list) {
         list = _list;
+
+        PrinterWindow = new JFrame();
+        PrinterWindow.setTitle("Printer");
+        PrinterWindow.setSize(500, 150);
+        PrinterWindow.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        PrinterWindow.setLayout(new GridBagLayout());
+//        PrinterWindow.setLayout(new BoxLayout(PrinterWindow, BoxLayout.Y_AXIS));
+
+        output = new TextArea("");
+        output.setEditable(false);
+
+        PrinterWindow.add(output);
     }
 
     /**
@@ -26,14 +45,22 @@ class Printer implements HardwareInterface_Printer {
      */
     public void print() {
         double total = 0.0;
+        StringBuilder stringOutput = new StringBuilder();
         for (ReceiptItem item : list) {
-            System.out.println(item);
+            stringOutput.append(item.toString());
+            stringOutput.append("\n");
+//            System.out.println(item);
             total += item.getTotal();
         }
-        System.out.println("____________________________________________");
-        System.out.printf("Subtotal %-10.2f\n", total);
-        System.out.printf("Tax      %-10.2f\n", (taxRate * total));
-        System.out.printf("Total    %-10.2f\n", (total + (taxRate * total)));
-        System.out.println("____________________________________________");
+
+        stringOutput.append("____________________________________________\n");
+        stringOutput.append(String.format("Subtotal %-10.2f\n", total));
+        stringOutput.append(String.format("Tax      %-10.2f\n", (taxRate * total)));
+        stringOutput.append(String.format("Total    %-10.2f\n", (total + (taxRate * total))));
+        stringOutput.append("____________________________________________");
+
+        output.setText(stringOutput.toString());
+
+        PrinterWindow.setVisible(true);
     }
 }
