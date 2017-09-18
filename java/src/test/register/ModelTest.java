4f7664d804fd94c31219348be06513eb567591a0
Adam Sinck <absinck+github@grueslayer.com>
2017-09-18 16:23:45 -0700

    add unit tests for the Model: the database connection

--- java/src/test/register/ModelTest.java
+++ java/src/test/register/ModelTest.java
@@ -1,13 +1,23 @@
 package register;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ModelTest {
-    private Model model;
+    @Test
+    public void testModelItem() {
+        // This makes sure that when an item is looked up in the database that it returns the
+        // expected values.
+        Model model = new Model();
+        ReceiptItem item = model.itemLookup(4011);
+        assertEquals(item, new ReceiptItem("BANANAS", 0, 82.04));
+        model.destruct();
+        System.out.println("All Model Item tests passed.");
+    }
 
     @Test
-    public void testModel() {
-        model = new Model();
+    public void testModelCustomer() {
 
         /*
         This needs to test all database functionality, for both customer and item.
@@ -36,6 +46,67 @@ public class ModelTest {
          odd constraints on the database - only that certain keys are not null.
          */
 
-        System.out.println("All tests passed.");
+        Model model = new Model();
+
+        // Test to make sure that the model can add a customer to the database.
+        // These use negative numbers for the phone numbers, because those are invalid numbers and
+        // won't exist in the database (in theory...).
+        // TODO: make a unit test for the customer input fields to make sure that it only inputs
+        // TODO: "valid" phone numbers
+
+        Customer c1 = new Customer("Model Test 1 Street", "Model Test 1 L2",
+                "Test1", "confusion", 12345, "user1@example.com",
+                -1, false, true);
+        Customer c2 = new Customer("Model Test 2 Street", "Model Test 2 L2",
+                "Test2", "none", 54321, "user2@example.com",
+                -10, true, false);
+        // This has the same phone number as c2, but different other fields. It should not be
+        // insertable into the database.
+        Customer c3 = new Customer("Model Test 3 Street", "Model Test 3 L2",
+                "Test3", "none", 72984010, "user3@example.com",
+                -10, true, false);
+
+
+        // If either of these returns a customer, then there's an issue somewhere. These customers
+        // should not exist.
+        assertEquals(model.customerLookup(c1), null);
+        assertEquals(model.customerLookup(c2), null);
+
+        // Add a customer and make sure that it gets back from the database successfully
+        model.addCustomer(c1);
+        assertEquals(model.customerLookup(c1), c1);
+        assertNotEquals(model.customerLookup(c1), c2);
+
+        // Delete the customer and make sure it's gone
+        model.deleteCustomer(c1);
+        assertEquals(model.customerLookup(c1), null);
+
+
+        // Test persistence
+        // We'll put c2 into the database, and then close the connection and form a new one. If
+        // changes are not committed to the database they'll only last for the session.
+        model.addCustomer(c2);
+        assertEquals(model.customerLookup(c2), c2);
+        model.destruct();
+        model = new Model();
+        assertEquals(model.customerLookup(c2), c2);
+
+        // Test to make sure that customers with duplicate keys are not inserted into the database
+        model.addCustomer(c3);
+        assertEquals(model.customerLookup(c3), c2);
+
+        //clean up
+        model.deleteCustomer(c1);
+        model.deleteCustomer(c2);
+        model.deleteCustomer(c3);
+
+        //test while we're here
+        assertEquals(model.customerLookup(c1), null);
+        assertEquals(model.customerLookup(c2), null);
+        assertEquals(model.customerLookup(c3), null);
+
+        //close the connection
+        model.destruct();
+        System.out.println("All Model Customer tests passed.");
     }
 }
